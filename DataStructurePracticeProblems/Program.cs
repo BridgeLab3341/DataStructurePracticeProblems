b27757799ae48436768c793d45f23616f75bf932
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-20 08:16:34 +0530

    [somu]Add:Creating and Adding Stacks Using Generic.

--- DataStructurePracticeProblems/Program.cs
+++ DataStructurePracticeProblems/Program.cs
@@ -4,11 +4,17 @@
     {
         public static void Main(string[] args)
         {
-            LinkedList<int> list = new LinkedList<int>();
-            list.Add(1);
-            list.Add(2);
-            list.Add(3);
-            list.Display();
+            //LinkedList<int> list = new LinkedList<int>();
+            //list.Add(1);
+            //list.Add(2);
+            //list.Add(3);
+            //list.Display();
+            Stack<int>stacks= new Stack<int>();
+            stacks.Push(1);
+            stacks.Push(2);
+            stacks.Push(3);
+            stacks.Display();
+           // stacks.Pop();
         }
     }
 }
---
+++ DataStructurePracticeProblems/Stacks.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DataStructurePracticeProblems
+{
+    internal class Stacks<T>
+    {
+        private Node<T> top;
+        public Stacks()
+        {
+            this.top = null;
+        }
+        public void Push(T value)
+        {
+            Node<T> node = new Node<T>(value);
+            if (this.top == null)
+            {
+                node.Next = null;
+            }
+            else
+            {
+                node.Next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} is Pushed to Stack", value);
+        }
+        public void Display()
+        {
+            Node<T> temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.Next;
+            }
+        }
+        public void Peak()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack Is Empty");
+            }
+            Console.WriteLine("{0} is in the top of the Stack", this.top.data);
+        }
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is Empty,Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the Stack", this.top.data);
+            this.top = this.top.Next;
+        }
+    }
+}
