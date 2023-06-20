4fa1a62381e8819b7172e01e84010ed26a36ff33
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-20 08:28:54 +0530

    [somu]Add:Creating Queues Using Generics.

--- DataStructurePracticeProblems/Program.cs
+++ DataStructurePracticeProblems/Program.cs
@@ -9,12 +9,19 @@
             //list.Add(2);
             //list.Add(3);
             //list.Display();
-            Stack<int>stacks= new Stack<int>();
-            stacks.Push(1);
-            stacks.Push(2);
-            stacks.Push(3);
-            stacks.Display();
+            //Stack<int>stacks= new Stack<int>();
+            //stacks.Push(1);
+            //stacks.Push(2);
+            //stacks.Push(3);
+            //stacks.Display();
            // stacks.Pop();
+           Queues<int>queues = new Queues<int>();
+            queues.Enqueue(1);
+            queues.Enqueue(2);
+            queues.Enqueue(3);
+            queues.Display();
+            queues.Dequeue();
+            queues.Display();
         }
     }
 }
---
+++ DataStructurePracticeProblems/Queues.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DataStructurePracticeProblems
+{
+    internal class Queues<T>
+    {
+        Node<T> head = null;
+        public void Enqueue(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node<T> temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine("{0} inserted into Queue", node.data);
+        }
+        public void Display()
+        {
+            Node<T> temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("linkedlist is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.Next;
+            }
+        }
+        public void Dequeue()
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Queue is empty,deleltion is not Possible");
+                return;
+            }
+            else
+            {
+                while (this.head != null)
+                {
+                    Console.WriteLine("value Dequeued is {0}", this.head.data);
+                    this.head = this.head.Next;
+                }
+            }
+        }
+    }
+}
